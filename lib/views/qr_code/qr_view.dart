6d9cd36146f3171a79680fecbd31b462a4b99be0
Samilly <samillynunes19@gmail.com>
2022-06-04 16:25:11 -0300

    Qr view updated, and logo  extension updated

--- lib/views/qr_code/qr_view.dart
+++ lib/views/qr_code/qr_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_extension/constants.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QRView extends StatefulWidget {
   const QRView({Key? key}) : super(key: key);
@@ -14,6 +16,14 @@ class _QRViewState extends State<QRView> {
   int qrColorIndex = 0;
   int qrBackgroundColorIndex = 0;
 
+  final InputBorder _inputBorder = OutlineInputBorder(
+    borderSide: const BorderSide(
+      color: Colors.black,
+      width: 2,
+    ),
+    borderRadius: BorderRadius.circular(16),
+  );
+
   @override
   void initState() {
     _textController = TextEditingController(text: qrText);
@@ -24,6 +34,136 @@ class _QRViewState extends State<QRView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 24,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: QrImage(
+                data: qrText,
+                padding: const EdgeInsets.all(16),
+                backgroundColor: qrBackgroundColors[qrBackgroundColorIndex],
+                foregroundColor: qrColors[qrColorIndex],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 16,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: _textController,
+                      focusNode: _textFocus,
+                      decoration: InputDecoration(
+                        labelText: 'QR Text',
+                        labelStyle: const TextStyle(color: Color(0xFF80919F)),
+                        hintText: 'Enter text / URL',
+                        hintStyle: const TextStyle(color: Color(0xFF80919F)),
+                        enabledBorder: _inputBorder,
+                        focusedBorder: _inputBorder,
+                      ),
+                      onChanged: (value) => setState(() {
+                        qrText = value;
+                      }),
+                    ),
+                    const SizedBox(height: 24),
+                    const Text(
+                      'Choose QR Color',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.separated(
+                        separatorBuilder: (_, __) => const SizedBox(width: 8),
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: qrColors.length,
+                        itemBuilder: (context, index) => InkWell(
+                          hoverColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          onTap: () => setState(() {
+                            qrColorIndex = index;
+                          }),
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              CircleAvatar(
+                                radius: qrColorIndex == index ? 23 : 22,
+                                backgroundColor: qrColorIndex == index
+                                    ? Colors.black
+                                    : Colors.black26,
+                              ),
+                              CircleAvatar(
+                                radius: 20,
+                                backgroundColor: qrColors[index],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      'Choose QR Background Color',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.separated(
+                        separatorBuilder: (_, __) => const SizedBox(width: 8),
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: qrBackgroundColors.length,
+                        itemBuilder: (context, index) => InkWell(
+                          hoverColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          onTap: () => setState(() {
+                            qrBackgroundColorIndex = index;
+                          }),
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              CircleAvatar(
+                                radius:
+                                    qrBackgroundColorIndex == index ? 23 : 22,
+                                backgroundColor: qrBackgroundColorIndex == index
+                                    ? Colors.black
+                                    : Colors.black26,
+                              ),
+                              CircleAvatar(
+                                radius: 20,
+                                backgroundColor: qrBackgroundColors[index],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
